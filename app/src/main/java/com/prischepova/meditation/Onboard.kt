cc45f4ff86f14423587eaac73d5ca5d94121f12a
Kseniya <prii.sschepovva@yandex.ru>
2022-12-13 23:40:58 +0300

    add meditation

---
+++ app/src/main/java/com/prischepova/meditation/Onboard.kt
@@ -0,0 +1,11 @@
+package com.prischepova.meditation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Onboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboard)
+    }
+}
